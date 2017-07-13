40f86f3434ca0a9011ddcfdb223ffefa7931619a
Sijun Liu <sijliu@microsoft.com>
2017-07-12 17:20:05 -0700

    build on linux

--- build.sh
+++ build.sh
@@ -1,46 +1,24 @@
 #!/usr/bin/env bash
-repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $repoFolder
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
-if [ ! -z $KOREBUILD_ZIP ]; then
-    koreBuildZip=$KOREBUILD_ZIP
-fi
+echo "#############################################################"
+echo "Restore"
+echo "#############################################################"
+dotnet restore WilsonLinux.sln
 
-buildFolder=".build"
-buildFile="$buildFolder/KoreBuild.sh"
+echo "#############################################################"
+echo "Build"
+echo "#############################################################"
+dotnet build WilsonLinux.sln
 
-if test ! -d $buildFolder; then
-    echo "Downloading KoreBuild from $koreBuildZip"
-    
-    tempFolder="/tmp/KoreBuild-$(uuidgen)"    
-    mkdir $tempFolder
-    
-    localZipFile="$tempFolder/korebuild.zip"
-    
-    retries=6
-    until (wget -O $localZipFile $koreBuildZip 2>/dev/null || curl -o $localZipFile --location $koreBuildZip 2>/dev/null)
-    do
-        echo "Failed to download '$koreBuildZip'"
-        if [ "$retries" -le 0 ]; then
-            exit 1
-        fi
-        retries=$((retries - 1))
-        echo "Waiting 10 seconds before retrying. Retries left: $retries"
-        sleep 10s
-    done
-    
-    unzip -q -d $tempFolder $localZipFile
-  
-    mkdir $buildFolder
-    cp -r $tempFolder/**/build/** $buildFolder
-    
-    chmod +x $buildFile
-    
-    # Cleanup
-    if test ! -d $tempFolder; then
-        rm -rf $tempFolder  
-    fi
-fi
+echo "#############################################################"
+echo "Pack"
+echo "#############################################################"
+dotnet pack WilsonLinux.sln
 
-$buildFile -r $repoFolder "$@"
+# copy all nuget packages to artifacts folder
+echo "#############################################################"
+echo "Move nuget packages to artifacts folder"
+echo "#############################################################"
+rm -rf artifacts
+mkdir artifacts
+mv src/*/bin/Debug/*.nupkg artifacts/
